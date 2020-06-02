1834bf8a31ae9a3f4adb68d3fa4f6c6914d187ab
Malebestia <malebestia@yahoo.it>
2020-06-02 13:29:44 +0200

    .

---
+++ laravel/storage/framework/smarty/compile/e83d623db7d9bfec27226a93a79e686a8570853d_0.file.header.tpl.php
@@ -0,0 +1,27 @@
+<?php
+/* Smarty version 3.1.36, created on 2020-06-02 13:25:27
+  from 'E:\phpWorkspace\food_tribu_project\base_test\laravel\resources\views\themes\fipitaly\home\header.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.36',
+  'unifunc' => 'content_5ed63727d9bb34_96408507',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'e83d623db7d9bfec27226a93a79e686a8570853d' => 
+    array (
+      0 => 'E:\\phpWorkspace\\food_tribu_project\\base_test\\laravel\\resources\\views\\themes\\fipitaly\\home\\header.tpl',
+      1 => 1591097091,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:../slot/_header.tpl' => 1,
+  ),
+),false)) {
+function content_5ed63727d9bb34_96408507 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender('file:../slot/_header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+}
+}
