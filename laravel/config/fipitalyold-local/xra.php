ae75a0200475f92d7c243c1d72688a435849701f
Malebestia <malebestia@yahoo.it>
2020-06-05 19:27:07 +0200

    .

---
+++ laravel/config/fipitalyold-local/xra.php
@@ -0,0 +1,15 @@
+<?php
+
+$def = [
+    'pub_theme' => 'fipitaly',
+    'adm_theme' => 'sb-admin-2-bs4',
+    'enable_ads' => '1',
+    'model' => [
+        'home' => 'Modules\Blog\Models\Home',
+    ],
+    'primary_lang' => 'it',
+    'show_trans_key' => false,
+    'disable_frontend_dynamic_route' => true,
+];
+
+return $def;
