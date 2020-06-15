e87c69ec781f537cdfbee0e76d768e29cf657f97
Malebestia <malebestia@yahoo.it>
2020-06-15 21:01:31 +0200

    .

--- laravel/config/bepi-local/xra.php
+++ laravel/config/bepi-local/xra.php
@@ -1,13 +1,13 @@
-<?php
-
- return [
-     'adm_theme' => 'sb-admin-2-bs4',
-     'enable_ads' => '1',
-     'model' => [
-         'article' => 'Modules\Blog\Models\Article',
-         'home' => 'Modules\Xot\Models\Home',
-     ],
-     'primary_lang' => 'it',
-     'pub_theme' => 'bsitalia',
-     'show_trans_key' => false,
- ];
+<?php return array (
+  'adm_theme' => 'sb-admin-2-bs4',
+  'enable_ads' => '1',
+  'model' => 
+  array (
+    'article' => 'Modules\Blog\Models\Article',
+    'home' => 'Modules\Xot\Models\Home',
+    'profile' => 'Modules\Food\Models\Profile',
+  ),
+  'primary_lang' => 'it',
+  'pub_theme' => 'miniblog',
+  'show_trans_key' => false,
+);
