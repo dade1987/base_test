2b459345c973dc2efea229a4d6b14cc0901cb994
Malebestia <malebestia@yahoo.it>
2020-06-16 21:51:58 +0200

    .

--- laravel/config/bepi-local/xra.php
+++ laravel/config/bepi-local/xra.php
@@ -1,14 +1,17 @@
-<?php return array (
-  'adm_theme' => 'sb-admin-2-bs4',
-  'enable_ads' => '1',
-  'model' => 
-  array (
-    'article' => 'Modules\Blog\Models\Article',
-    'category' => 'Modules\Blog\Models\Category',
-    'home' => 'Modules\Xot\Models\Home',
-    'profile' => 'Modules\Food\Models\Profile',
-  ),
-  'primary_lang' => 'it',
-  'pub_theme' => 'miniblog',
-  'show_trans_key' => false,
-);
+<?php
+
+ return [
+     'adm_theme' => 'sb-admin-2-bs4',
+     'enable_ads' => '1',
+     'model' => [
+         'article' => 'Modules\Blog\Models\Article',
+         'category' => 'Modules\Blog\Models\Category',
+         'profile' => 'Modules\Blog\Models\Profile',
+         'home' => 'Modules\Blog\Models\Home',
+     ],
+     'primary_lang' => 'it',
+     'pub_theme' => 'miniblog',
+
+     'show_trans_key' => true,
+     'forcessl' => false,
+ ];
