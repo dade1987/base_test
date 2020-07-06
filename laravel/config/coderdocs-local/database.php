d04088f84edd68bbca8f1ea6aa79cea422308b17
Malebestia <malebestia@yahoo.it>
2020-07-06 12:17:18 +0200

    .

--- laravel/config/coderdocs-local/database.php
+++ laravel/config/coderdocs-local/database.php
@@ -6,7 +6,7 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
+            'database' => env('DB_DATABASE_blog', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             //'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
