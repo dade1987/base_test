82fc47de155377a71141f7f8df74648c1789b608
Malebestia <malebestia@yahoo.it>
2020-07-10 20:55:20 +0200

    .

--- laravel/config/localhost/xra.php
+++ laravel/config/localhost/xra.php
@@ -3,10 +3,15 @@
   'enable_ads' => '1',
   'model' => 
   array (
+    'bell_boy' => 'Modules\Food\Models\BellBoy',
+    'cart' => 'Modules\Cart\Models\Cart',
+    'cuisine_cat' => 'Modules\Food\Models\CuisineCat',
     'home' => 'Modules\Xot\Models\Home',
     'location' => 'Modules\Food\Models\Location',
+    'page' => 'Modules\Blog\Models\Page',
     'profile' => 'Modules\Food\Models\Profile',
     'restaurant' => 'Modules\Food\Models\Restaurant',
+    'tip' => 'Modules\Food\Models\Tip',
     'widget' => 'Modules\Xot\Models\Widget',
   ),
   'primary_lang' => 'it',
