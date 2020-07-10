5c255a8e6dcd72b40290b91ece8878ff18f83626
Malebestia <malebestia@yahoo.it>
2020-07-10 13:00:07 +0200

    .

--- laravel/config/localhost/xra.php
+++ laravel/config/localhost/xra.php
@@ -1,11 +1,15 @@
-<?php
-
- return [
-     'adm_theme' => 'sb-admin-2-bs4',
-     'enable_ads' => '1',
-     'model' => [
-     ],
-     'primary_lang' => 'it',
-     'pub_theme' => 'directorybs4',
-     'show_trans_key' => true,
- ];
+<?php return array (
+  'adm_theme' => 'sb-admin-2-bs4',
+  'enable_ads' => '1',
+  'model' => 
+  array (
+    'home' => 'Modules\Xot\Models\Home',
+    'location' => 'Modules\Food\Models\Location',
+    'profile' => 'Modules\Food\Models\Profile',
+    'restaurant' => 'Modules\Food\Models\Restaurant',
+    'widget' => 'Modules\Xot\Models\Widget',
+  ),
+  'primary_lang' => 'it',
+  'pub_theme' => 'directorybs4',
+  'show_trans_key' => true,
+);
