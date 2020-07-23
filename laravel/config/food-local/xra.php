68dcaa3e5f1a380dd2039ac4701afd68258bc600
Malebestia <malebestia@yahoo.it>
2020-07-23 11:19:52 +0200

    .

--- laravel/config/food-local/xra.php
+++ laravel/config/food-local/xra.php
@@ -1,85 +1,77 @@
-<?php return array (
-  'adm_theme' => 'adminflu',
-  'enable_ads' => '1',
-  'enable_packs' => 
-  array (
-    0 => 'backend',
-    1 => 'blog',
-    2 => 'extend',
-    3 => 'food',
-    4 => 'frontend',
-    5 => 'import',
-    6 => 'lu',
-    7 => 'settings',
-    8 => 'xra',
-  ),
-  'forcessl' => false,
-  'migrate_packs' => 
-  array (
-    0 => 'backend',
-    1 => 'blog',
-    2 => 'food',
-    3 => 'import',
-    4 => 'lu',
-    5 => 'seo',
-    6 => 'xra',
-  ),
-  'model' => 
-  array (
-    'amenity' => 'Modules\Blog\Models\Amenity',
-    'article' => 'Modules\Blog\Models\Article',
-    'bell_boy' => 'Modules\Food\Models\BellBoy',
-    'cart' => 'Modules\Cart\Models\Cart',
-    'category' => 'Modules\Blog\Models\Category',
-    'cuisine' => 'Modules\Food\Models\Cuisine',
-    'cuisine_cat' => 'Modules\Food\Models\CuisineCat',
-    'home' => 'Modules\Blog\Models\Home',
-    'ingredient' => 'Modules\Food\Models\Ingredient',
-    'ingredient_cat' => 'Modules\Food\Models\IngredientCat',
-    'location' => 'Modules\Food\Models\Location',
-    'opening_hour' => 'Modules\Food\Models\OpeningHour',
-    'page' => 'Modules\Blog\Models\Page',
-    'photo' => 'Modules\Blog\Models\Photo',
-    'post' => 'Modules\Blog\Models\Post',
-    'profile' => 'Modules\Food\Models\Profile',
-    'rating' => 'Modules\Blog\Models\Rating',
-    'recipe' => 'Modules\Food\Models\Recipe',
-    'restaurant' => 'Modules\Food\Models\Restaurant',
-    'restaurant_owner' => 'Modules\Food\Models\RestaurantOwner',
-    'tip' => 'Modules\Food\Models\Tip',
-    'waiter' => 'Modules\Food\Models\Waiter',
-    'widget' => 'Modules\Xot\Models\Widget',
-  ),
-  'primary_lang' => 'it',
-  'pub_theme' => 'directorybs4',
-  'related' => 
-  array (
-    'cuisine' => 
-    array (
-      0 => 'ingredientCat',
-      1 => 'recipe',
-    ),
-    'home' => 
-    array (
-    ),
-    'ingredientCat' => 
-    array (
-      0 => 'ingredient',
-    ),
-    'postCat' => 
-    array (
-      0 => 'post',
-    ),
-    'restaurant' => 
-    array (
-      0 => 'cuisine',
-      1 => 'cuisineCat',
-    ),
-  ),
-  'roots' => 
-  array (
-    0 => 'profile',
-    1 => 'restaurant',
-  ),
-  'show_trans_key' => true,
-);
+<?php
+
+ return [
+     'adm_theme' => 'adminflu',
+     'enable_ads' => '1',
+     'enable_packs' => [
+         0 => 'backend',
+         1 => 'blog',
+         2 => 'extend',
+         3 => 'food',
+         4 => 'frontend',
+         5 => 'import',
+         6 => 'lu',
+         7 => 'settings',
+         8 => 'xra',
+     ],
+     'forcessl' => false,
+     'migrate_packs' => [
+         0 => 'backend',
+         1 => 'blog',
+         2 => 'food',
+         3 => 'import',
+         4 => 'lu',
+         5 => 'seo',
+         6 => 'xra',
+     ],
+     'model' => [
+         'amenity' => 'Modules\Blog\Models\Amenity',
+         'article' => 'Modules\Blog\Models\Article',
+         'bell_boy' => 'Modules\Food\Models\BellBoy',
+         'cart' => 'Modules\Cart\Models\Cart',
+         'category' => 'Modules\Blog\Models\Category',
+         'cuisine' => 'Modules\Food\Models\Cuisine',
+         'cuisine_cat' => 'Modules\Food\Models\CuisineCat',
+         'home' => 'Modules\Blog\Models\Home',
+         'ingredient' => 'Modules\Food\Models\Ingredient',
+         'ingredient_cat' => 'Modules\Food\Models\IngredientCat',
+         'location' => 'Modules\Food\Models\Location',
+         'opening_hour' => 'Modules\Food\Models\OpeningHour',
+         'page' => 'Modules\Blog\Models\Page',
+         'photo' => 'Modules\Blog\Models\Photo',
+         'post' => 'Modules\Blog\Models\Post',
+         'profile' => 'Modules\Food\Models\Profile',
+         'rating' => 'Modules\Blog\Models\Rating',
+         'recipe' => 'Modules\Food\Models\Recipe',
+         'restaurant' => 'Modules\Food\Models\Restaurant',
+         'restaurant_owner' => 'Modules\Food\Models\RestaurantOwner',
+         'tip' => 'Modules\Food\Models\Tip',
+         'waiter' => 'Modules\Food\Models\Waiter',
+         'widget' => 'Modules\Xot\Models\Widget',
+     ],
+     'primary_lang' => 'it',
+     'pub_theme' => 'directorybs4',
+     'related' => [
+         'cuisine' => [
+             0 => 'ingredientCat',
+             1 => 'recipe',
+         ],
+         'home' => [
+         ],
+         'ingredientCat' => [
+             0 => 'ingredient',
+         ],
+         'postCat' => [
+             0 => 'post',
+         ],
+         'restaurant' => [
+             0 => 'cuisine',
+             1 => 'cuisineCat',
+         ],
+     ],
+     'roots' => [
+         0 => 'profile',
+         1 => 'restaurant',
+     ],
+     'show_trans_key' => true,
+ ];
